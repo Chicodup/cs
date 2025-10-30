cfbc103b94a61ae289107e32b764940f1ec32769
Chicodup <user.nazar.com@gmail.com>
2025-10-30 20:34:07 +0200

    gggggg

--- book/settings.py
+++ book/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_system',
     'cs2_app',
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth_system.User'
